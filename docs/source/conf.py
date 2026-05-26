f1438b5c4836bffd0a98861b189af763d63e4925
Thomas.Villani <thomas.villani@gmail.com>
2026-05-26 16:01:46 -0400

    Bump version: 0.6.3 → 0.6.4

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -7,7 +7,7 @@
 project = "nb"
 copyright = "2025"
 author = "Thomas Villani"
-release = "0.6.3"
+release = "0.6.4"
 
 # -- General configuration ---------------------------------------------------
 extensions = []
--- nb/__init__.py
+++ nb/__init__.py
@@ -1,3 +1,3 @@
 """nb - A plaintext-first note-taking and todo management CLI."""
 
-__version__ = "0.6.3"
+__version__ = "0.6.4"
