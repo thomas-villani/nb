f96cf6a0aee8203edd1ed5d414e6d0d839a2684d
Thomas.Villani <thomas.villani@gmail.com>
2026-06-16 10:20:04 -0400

    Bump version: 0.6.6 → 0.6.7

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -7,7 +7,7 @@
 project = "nb"
 copyright = "2025"
 author = "Thomas Villani"
-release = "0.6.6"
+release = "0.6.7"
 
 # -- General configuration ---------------------------------------------------
 extensions = []
